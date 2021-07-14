3304cf967d1450badda0d3b1e0f553982c558892
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-14 14:47:25 -0400

    made Post model

---
+++ models/Post.js
@@ -0,0 +1,20 @@
+const {Model, Datatypes} = require("sequelize")
+const sequelize= require("../config/connection")
+
+class Post extends Model{}
+
+Post.init(
+    {
+        title: Datatypes.STRING,
+        body: Datatypes.STRING
+    },
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+    }
+)
+
+module.exports = Post
