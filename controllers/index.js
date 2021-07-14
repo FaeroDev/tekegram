aaae07184e4c78c5168a599b6e20f740959cf2e2
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-14 17:09:50 -0400

    got api routes to connect....only get route made

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 // const express = require('express')
 // const router = express.Router()
 const router = require('express').Router();
-// const apiRoutes = require('./api/');
-const homeRoutes = require('./view/homeRoutes');
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeroutes');
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 // router.use('/', (req, res) => {
 //     res.send("<h1>index route</h1>")
