e91d1f6ba9af1c5e0abcfa05955957e259e39123
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-15 12:29:17 -0400

    after research anf looking at old project can reuse some code from it......started making viewws

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,17 +1,88 @@
+// const router = require("express").Router();
+// const { Post, User } = require("../models");
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const postData = await Post.findAll({
+//       include: [User],
+//     });
+
+//     const posts = postData.map((post) => post.get({ plain: true }));
+//     res.render("homepage", { posts });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// module.exports = router;
+
+
 const router = require("express").Router();
 const { Post, User } = require("../models");
 
+// GET all galleries for homepage
 router.get("/", async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      include: [User],
+    const dbPictureData = await Post.findAll({
+      // include: [
+      //   {
+      //     model: Phrase,
+      //     attributes: ["id", "upper_text", "lower_text", "picture_id"],
+      //   },
+      // ],
     });
 
-    const posts = postData.map((post) => post.get({ plain: true }));
-    res.render("homepage", { posts });
+    const pictures = dbPictureData.map((picture) =>
+      picture.get({ plain: true })
+    );
+    res.render("homepage", {
+      pictures,
+      loggedIn: req.session.loggedIn,
+    });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
 
+// GET one picture
+router.get("/picture/:id", async (req, res) => {
+  try {
+    const dbPictureData = await Picture.findByPk(req.params.id, {
+      include: [
+        {
+          model: Phrase,
+          attributes: ["id", "upper_text", "lower_text", "picture_id"],
+        },
+      ],
+    });
+
+    const picture = dbPictureData.get({ plain: true });
+    res.render("picture", { picture,
+       loggedIn: req.session.loggedIn 
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/phrase/:id", async (req, res) => {
+  try {
+    const dbPhraseData = await phrase.findbyFK(req.params.id);
+
+    const phrase = dbPhraseData.get({ plain: true });
+    res.render("phrase", { phrase });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
 module.exports = router;
+
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-// const routes = require("./controllers");
+const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -37,7 +37,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+// app.use(require('./controllers/'));
+app.use(routes)
 
 app.listen(PORT, () =>{
   console.log("connected")
