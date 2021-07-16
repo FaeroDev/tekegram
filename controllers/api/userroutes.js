19c2364c60053d162d77664ecd9fa691afc60bbb
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-16 10:54:59 -0400

    create new user now works and login validates password with created hash...seeded logins no longer work due to no hash

--- controllers/api/userroutes.js
+++ controllers/api/userroutes.js
@@ -60,14 +60,14 @@ router.post("/login", async (req, res) => {
       return;
     }
 
-    // const validPassword = await dbUserData.checkPassword(req.body.password);
+    const validPassword = await dbUserData.checkPassword(req.body.password);
 
-    // if (!validPassword) {
-    //   res
-    //     .status(400)
-    //     .json({ message: "Incorrect email or password. Please try again!" });
-    //   return;
-    // };
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    };
 
     
     
--- public/js/login.js
+++ public/js/login.js
@@ -28,7 +28,7 @@ const loginForm = async (event) => {
     const email = document.getElementById("signup-email").value.trim();
     const password = document.getElementById("signup-password").value.trim();
     if (username && email && password) {
-      const response = await fetch("/api/users", {
+      const response = await fetch("/api/user", {
         method: "POST",
         body: JSON.stringify({ username, email, password }),
         headers: { "Content-Type": "application/json" },
